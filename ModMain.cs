748de7fad2e20892a7e77f613e0f0dc6ec8d17b1
yamiM0NSTER <jomex50@gmail.com>
2022-08-13 03:05:20 +0200

    Generate Project

---
+++ ModMain.cs
@@ -0,0 +1,12 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CVR.MirrorsMod {
+  public class ModMain : MelonMod {
+  }
+}
